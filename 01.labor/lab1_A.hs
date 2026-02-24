3b9f7ba7508c8c27b3db7218afeed485fa1ba886
DaniB555 <bartos.daniel@student.ms.sapientia.ro>
2026-02-24 14:13:21 +0200

    lab2

--- 01.labor/lab1_A.hs
+++ 01.labor/lab1_A.hs
@@ -1,3 +1,7 @@
+import Text.Printf (printf)
+import Language.Haskell.TH (prim)
+import Distribution.Simple.Setup (falseArg)
+import System.Win32 (LOCALESIGNATURE(lsCsbDefault))
 
 osszeg :: Num a => a -> a -> a
 osszeg a b = a + b
@@ -41,3 +45,42 @@ min2 a b
     |a<b =a 
     |otherwise =b
 
+negyzetgyokN n = [ sqrt i|i<-[1..n]]
+
+negyzetN n = [i*i| i <- [1..n] ]
+
+kobN n = [i^3 | i <-[1..n]]
+
+nemNegyzetN n=[i|i<-[1..n],(sqrt i)*(sqrt i)/=i]
+
+xhatvanyN x n =[x^i|i<-[1..n]]
+
+
+osztokparosN n=[i|i<-[1..n],n `mod` i == 0,mod i 2 ==0]
+
+osztokN n=[i|i<-[1..n],mod n i ==0]
+primszam n = osztokN n==[1,n]
+
+primszamokN n=[i|i<-[1..n], primszam i]
+
+osszetettN n=[i|i<-[1..n],primszam i == False]
+osszetettparosN n=[i|i<-[1..n],primszam i == False,mod i 2 ==0]
+osszetettparatlanN n=[i|i<-[1..n],primszam i == False,mod i 2 /=0]
+pitagorasz n=[(a,b,c)| c<-[1..n],b<-[1..c],a<-[1..b] , a**2+b**2==c**2]
+
+betuSzam =zip['a'..'z'][0..25]
+szamok1 =zip[0..5][5,4..0]
+szamok2 n=[(i,n-i)|i<-[0..n]]
+tfLs n=take n ls
+    where
+        ls=[True,False]++ls
+main = do
+    putStrLn("x hatvany n")
+    print(xhatvanyN 5 3) 
+    print(primszamokN 10)
+    print(osszetettN 10)
+    print(osszetettparosN 10)
+    print(osszetettparatlanN 10)
+    --putStrLn("Pitagoraszi szamharmasok"++ show(pitagorasz 100))
+    --print(pitagorasz 100)
+    print(szamok2 10)
